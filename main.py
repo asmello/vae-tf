cbcb986a8a08030e96434fde438fd757b93f0757
André Sá de Mello <asmello.br@gmail.com>
2016-10-24 18:05:10 -0400

    Proper command-line argument parsing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import argparse
 
 import numpy as np
 import tensorflow as tf
@@ -8,40 +9,27 @@ import tensorflow as tf
 from data import Data
 import vae
 
-
 ARCHITECTURE = [ None, # input length, leave it like this
                  500, 500, # intermediate encoding
                  2 ] # latent space dims
                 # 50]
 # (and symmetrically back out again)
 
-HYPERPARAMS = {
-    "batch_size": 128,
-    "learning_rate": 5E-4,
-    "dropout": 0.9,
-    "lambda_l2_reg": 1E-5,
-    "nonlinearity": tf.nn.elu,
-    "squashing": tf.nn.sigmoid
-}
-
-MAX_ITER = 2000#2**16
-MAX_EPOCHS = np.inf
-
 LOG_DIR = "./log"
 METAGRAPH_DIR = "./out"
 PLOTS_DIR = "./png"
 
-def main(to_reload=None):
-    data = Data('data/sentiment_set_norm.pickle')
-    ARCHITECTURE[0] = data.sample_size
+def main(hp, max_iter, max_epochs, filename=None, to_reload=None):
 
     if to_reload: # restore
-        v = vae.VAE(ARCHITECTURE, HYPERPARAMS, meta_graph=to_reload)
+        v = vae.VAE(ARCHITECTURE, hp, meta_graph=to_reload)
         print("Loaded!")
 
     else: # train
-        v = vae.VAE(ARCHITECTURE, HYPERPARAMS, log_dir=LOG_DIR)
-        v.train(data, max_iter=MAX_ITER, max_epochs=MAX_EPOCHS,
+        data = Data(filename)
+        ARCHITECTURE[0] = data.sample_size
+        v = vae.VAE(ARCHITECTURE, hp, log_dir=LOG_DIR)
+        v.train(data, max_iter=max_iter, max_epochs=max_epochs,
                 cross_validate=False, verbose=True, save=True,
                 outdir=METAGRAPH_DIR, plots_outdir=PLOTS_DIR,
                 plot_latent_over_time=False)
@@ -49,6 +37,7 @@ def main(to_reload=None):
 
     # all_plots(v, mnist)
 
+    return v
 
 if __name__ == "__main__":
     tf.reset_default_graph()
@@ -59,8 +48,35 @@ if __name__ == "__main__":
         except(FileExistsError):
             pass
 
-    try:
-        to_reload = sys.argv[1]
-        main(to_reload=to_reload)
-    except(IndexError):
-        main()
+    parser = argparse.ArgumentParser(description='Variational Autoencoder.')
+    parser.add_argument('--num_epochs', type=int, default=np.inf,
+                        help='Maximum number of epochs.')
+    parser.add_argument('-n', '--num_iters', type=int, default=2000,
+                        help='Maximum number of iterations.')
+    parser.add_argument('-b', '--batch_size', type=int, default=128,
+                        help='Batch size to use for training.')
+    parser.add_argument('-r', '--learning_rate', type=float, default=5e-4,
+                        help='Learning rate for optimizer.')
+    parser.add_argument('-p', '--dropout_prob', type=float, default=0.9,
+                        help='Keep probability for dropout in training.')
+    parser.add_argument('--lambda', type=float, default=1e-5, dest='lambda_',
+                        help='Lambda in L2 regularization.')
+    parser.add_argument('-i', '--input', help='Use pre-trained graph in file.')
+    parser.add_argument('-t', '--train', dest='datafile',
+                        help='Use pickled datafile for training.')
+    args = parser.parse_args()
+
+    hp = {
+        "batch_size": args.batch_size,
+        "learning_rate": args.learning_rate,
+        "dropout": args.dropout_prob,
+        "lambda_l2_reg": args.lambda_,
+        "nonlinearity": tf.nn.elu,
+        "squashing": tf.nn.sigmoid
+    }
+
+    if args.datafile or args.input:
+        model = main(hp, args.num_iters, args.num_epochs,
+                     to_reload=args.input, filename=args.datafile)
+    else:
+        print('No action specified. See --help.')
